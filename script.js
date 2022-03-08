5cbeefa4b8ba17bf9fa44f3366193948f872f9f6
Prabhjot Singh <pjsingh1359@gmail.com>
2022-03-07 22:06:22 -0800

    saving work

---
+++ script.js
@@ -0,0 +1,7 @@
+let Book = function(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+};
+
+let book1 = new Book("The Hobbit", "J.R.R. Tolkien", "123456789");
